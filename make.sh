c01628cbabf7cc732585e8fd7bd29093530e4929
Jeffrey Lee <jeffrey@wizpace.com>
2019-12-06 12:42:05 +0900

    Build

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 export GOPATH="$HOME/GolandProjects/linux-startup-send-ip-info/"
-GOOS=linux GOARCH=amd64 go build -v sendip -o sendip_amd64
-GOOS=linux GOARCH=arm go build -v sendip -o sendip_arm
-GOOS=linux GOARCH=arm64 go build -v sendip -o sendip_arm64
+GOOS=linux GOARCH=amd64 go build -v sendip
+mv "sendip" "sendip_amd64"
+GOOS=linux GOARCH=arm go build -v sendip
+mv "sendip" "sendip_arm"
+GOOS=linux GOARCH=arm64 go build -v sendip
+mv "sendip" "sendip_arm64"
