ada42d9859a3f79ef05ddbc9ab428ac940d657c2
Jeffrey Lee <jeffrey@wizpace.com>
2020-01-29 10:41:46 +0900

    Added more options && docs

--- make.sh
+++ make.sh
@@ -5,3 +5,7 @@ GOOS=linux GOARCH=arm go build -v sendip
 mv "sendip" "sendip_arm"
 GOOS=linux GOARCH=arm64 go build -v sendip
 mv "sendip" "sendip_arm64"
+GOOS=linux GOARCH=386 go build -v sendip
+mv "sendip" "sendip_386"
+GOOS=linux GOARCH=amd64p32 go build -v sendip
+mv "sendip" "sendip_amd64p32"
